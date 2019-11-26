068c1c0ab9d4afcb53899f7b7262c44e04b34630
eeveeshi <56522675+eeveeshi@users.noreply.github.com>
2019-11-26 16:32:59 -0500

    Add files via upload

---
+++ calcProb.py
@@ -0,0 +1,30 @@
+calcSProbs = [1.4970, 1.5167, 1.5364, 1.5561, 1.5758, 1.5955, 1.6153,
+1.6350, 1.6547, 1.6744, 1.6941, 1.7139, 1.7336, 1.7533, 1.7730, 1.7927,
+1.8105, 1.8303, 1.8499, 1.8697, 1.8894, 1.9189, 1.9485, 1.9781, 2.0046,
+2.0342, 2.0638, 2.0933, 2.1210, 2.1506, 2.1801, 2.2097, 2.2393, 2.2689,
+2.2984, 2.3281, 2.3576, 2.3872, 2.4168, 2.4444, 2.4741, 2.5036, 2.5332,
+2.5597, 2.5893, 2.6189, 2.6484, 2.6780, 2.7076, 2.7353, 2.7649, 2.7944,
+2.8240, 2.8536, 2.8832, 2.9128, 2.9424, 2.9719, 3.0015, 3.0311, 3.0588,
+3.0883, 3.1149, 3.1444, 3.1741, 3.2036, 3.2332, 3.2627, 3.2924, 3.3219,
+3.3515, 3.3792, 3.4088, 3.4384, 3.4679, 3.4976, 3.5272, 3.5568, 3.5864,
+3.6159, 3.6455, 3.6701, 3.6997, 3.7293, 3.7588, 3.7884, 3.8180, 3.8476,
+3.8772, 3.9068, 3.9364, 3.9660, 3.9936]
+
+cont = True
+while cont:
+    startLV = int(input("Starting Lucky Value?\n"))
+    numRolls = int(input("Number of rolls?\n"))
+    if not isinstance(startLV, int):
+        print("Not an integer or not positive. Cannot compute")
+        
+    elif startLV + numRolls > 93:
+        print("Not enough Lucky Value data yet!\n")
+    
+    else:        
+        probOfNotS = 1
+        
+        for i in range(numRolls):
+            probOfNotS *= (1-calcSProbs[startLV+1]/100)
+            
+        print("Probability of S-limited in %d rolls using calculated values, starting at %d Lucky Value: %0.02f%%\n" % (startLV, numRolls, 100-probOfNotS*100))
+        cont = not cont
---
+++ dispProb.py
@@ -0,0 +1,27 @@
+dispSProbs = [1.50, 1.52, 1.54, 1.56, 1.58, 1.60, 1.62, 1.64, 1.66, 1.68,
+1.70, 1.72, 1.74, 1.76, 1.78, 1.80, 1.82, 1.84, 1.86, 1.88, 1.90, 1.93,
+1.96, 1.99, 2.02, 2.05, 2.08, 2.11, 2.14, 2.17, 2.20, 2.23, 2.26, 2.29,
+2.32, 2.34, 2.37, 2.40, 2.43, 2.46, 2.49, 2.52, 2.55, 2.58, 2.61, 2.64,
+2.67, 2.70, 2.73, 2.76, 2.79, 2.82, 2.85, 2.88, 2.91, 2.94, 2.97, 3.00,
+3.03, 3.06, 3.09, 3.12, 3.15, 3.18, 3.21, 3.23, 3.26, 3.29, 3.32, 3.35,
+3.38, 3.41, 3.44, 3.47, 3.50, 3.53, 3.56, 3.59, 3.62, 3.65, 3.68, 3.71,
+3.74, 3.77, 3.80, 3.83, 3.86, 3.89, 3.92, 3.95, 3.98, 4.01, 4.04]
+
+cont = True
+while cont:
+    startLV = int(input("Starting Lucky Value?\n"))
+    numRolls = int(input("Number of rolls?\n"))
+    if not isinstance(startLV, int):
+        print("Not an integer or not positive. Cannot compute")
+        
+    elif startLV + numRolls > 93:
+        print("Not enough Lucky Value data yet!\n")
+    
+    else:        
+        probOfNotS = 1
+        
+        for i in range(numRolls):
+            probOfNotS *= (1-dispSProbs[startLV+1]/100)
+            
+        print("Probability of S-limited in %d rolls using in-game displayed values, starting at %d Lucky Value: %0.02f%%\n" % (startLV, numRolls, 100-probOfNotS*100))
+        cont = not cont
